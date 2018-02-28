3cb63ead2ed3e94cef2befcc6b834b011253ba2c
fajar <m.fajar2011@gmail.com>
2018-02-28 17:51:37 +0700

    add project

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdown;
+
+    private int waveNumber = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        waveCountdown.text = Mathf.Floor(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+         waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
